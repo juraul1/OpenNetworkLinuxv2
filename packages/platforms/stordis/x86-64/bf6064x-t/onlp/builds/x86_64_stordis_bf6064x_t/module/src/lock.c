ec03bed707047d324d7681058e4001133b07c384
CommitThis <gdavey@committhis.co.uk>
2021-10-12 12:35:47 +0100

    timedlock

--- packages/platforms/stordis/x86-64/bf6064x-t/onlp/builds/x86_64_stordis_bf6064x_t/module/src/lock.c
+++ packages/platforms/stordis/x86-64/bf6064x-t/onlp/builds/x86_64_stordis_bf6064x_t/module/src/lock.c
@@ -281,7 +281,12 @@ int bf6064x_lock_acquire()
     shared_lock_t * plock = bf6064_get_lock__();
     int rv = 0;
 
-    if ((rv = shared_lock_lock(plock)) 
+    struct timespec lock_wait_for;
+    lock_wait_for.tv_sec += 5;
+    
+    clock_gettime(CLOCK_REALTIME, &lock_wait_for);
+
+    if ((rv = shared_lock_timedlock(plock, &lock_wait_for)) 
             != SLOCK_ERROR_OK)
     {
         printf("Error acquiring shared lock: %s\n", shared_lock_strerror(rv));
